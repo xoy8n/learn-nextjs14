c8b0d04b679222b019610918ce89a1db686b916d
soyoon.seol <soyoon.seol@emotion.co.kr>
2024-09-26 14:26:23 +0900

    api 연결

---
+++ app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,6 @@
+//에러컴포넌트는 반드시 "use client"를 추가해야한다.
+"use client";
+
+export default function ErrorOMG() {
+  return <h1>OMG there's an error 🥺</h1>;
+}
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,26 @@
-export default function MovieDetail({
+import MovieVideos from "../../../../components/movie-videos";
+import MovieInfo from "../../../../components/movie-info";
+import { Suspense } from "react";
+//병렬적으로 fetch를 하는 방법
+//1. Promise.all()
+//2. <Suspense fallback={}></Suspense>
+//Page 단위 로딩 => loading.tsx
+//서버 컴포넌트 단위 로딩 => Suspense
+// To-do : suspense사용해서 스켈레톤 만들기
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>Movie {id}</h1>;
+  return (
+    <div>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
+    </div>
+  );
 }
