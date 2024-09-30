fd5ba7a5ac7f775cafe7dfddf1494d2ef68fbfc7
soyoon.seol <soyoon.seol@emotion.co.kr>
2024-09-30 14:27:09 +0900

    api 호출 (페이지 -> 컴포넌트)로 수정

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,5 +1,5 @@
-import { API_URL } from "../app/(home)/page";
 import styles from "../styles/movie-info.module.css";
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
 
 export async function getMovie(id: string) {
   const response = await fetch(`${API_URL}/${id}`);
